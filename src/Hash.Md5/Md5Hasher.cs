f3f1c2d595f172421114c0afebb42aa52860a570
Talha Sayed <talha_sayed@hotmail.com>
2022-03-22 18:34:26 +0300

    Incomplete code

---
+++ src/Hash.Md5/Md5Hasher.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Local
+
+namespace Hash.Md5;
+
+public class Md5Hasher
+{
+    private uint F(uint X, uint Y, uint Z) => (X & Y) | (~X & Z);
+    
+    private uint G(uint X, uint Y, uint Z) => (X & Z) | (Y & ~Z);
+    
+    private uint H(uint X, uint Y, uint Z) => X ^ Y ^ Z;
+    
+    private uint I(uint X, uint Y, uint Z) => Y ^ (X | ~Z);
+
+    public Md5Hasher()
+    {
+    }
+
+    public byte[] GenerateHash(byte[] message)
+    {
+        // length in bits
+        var length = message.Length * 8;
+
+        // Padding the message
+        var paddingChars = length % 512 == 0 ? 512 : length % 512;
+        var paddingBytes = paddingChars / 8;
+
+        var messageList = message.ToList();
+
+        messageList.Add(0b10000000);
+        paddingBytes--;
+
+        while (paddingBytes > 0)
+        {
+            messageList.Add(0b00000000);
+            paddingBytes--;
+        }
+        
+        // Initialize
+        uint A = 0x01234567;
+        uint B = 0x89abcdef;
+        uint C = 0xfedcba98;
+        uint D = 0x76543210;
+
+        var numBlocks = (messageList.Count * 8) / 512;
+        // main loop
+        // process 16 word blocks
+
+        for (var i =0; i < numBlocks; i++)
+        {
+            var AA = A;
+            var BB = B;
+            var CC = C;
+            var DD = D;
+
+
+            // Round 1
+
+            //A = Round1();
+
+
+
+
+        }
+
+        return message[..16];
+    }
+
+
+    private uint Round1(uint n1, uint n2, uint n3, byte k , byte s, byte i)
+    {
+        var x = BitOperations.RotateLeft(n1, s);
+
+        //var result = b + ((a + F(b, c, d) + X[k] + T[i]) <<< s)
+
+        return 0;
+    }
+}
---
+++ src/Hash.Md5/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Security.Cryptography;
+using Hash.Md5;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var hasher = new Md5Hasher();
+
+        var fileName = "C:\\talha\\inbox\\sample.jpg";
+
+        var builtinHash = BuiltinHasher(fileName);
+
+        var input = File.ReadAllBytes(fileName);
+        
+        var myHash = hasher.GenerateHash(input);
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.Write("Built-in hash: ");
+        DisplayHash(builtinHash);
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.Write("      My hash: ");
+        DisplayHash(myHash);
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+
+    public static byte[] BuiltinHasher(string fileName)
+    {
+        using var md5 = MD5.Create();
+        using var stream = File.OpenRead(fileName);
+        return md5.ComputeHash(stream);
+    }
+
+    public static void DisplayHash(byte[] bytes)
+    {
+        foreach (byte b in bytes)
+        {
+            Console.Write($"{b:X2} ");
+        }
+
+    }
+
+
+
+
+}
+
